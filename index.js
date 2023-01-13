796db758b4a433a206ec2dce0cd5538b7f0425ad
SpenUtt <spencerutt8@gmail.com>
2023-01-13 12:51:36 +0200

    added uuid and bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const bodyParser = require('body-parser');
 const express = require('express'),
       fs = require("fs"),
       path = require("path"),
-      morgan = require('morgan');
+      morgan = require('morgan'),
+      bodyParser = require('body-parser'),
+      uuid = require('uuid');
 const app = express();
 
 // Middlewares
