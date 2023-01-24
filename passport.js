7ba106a5a563bc52155d8004b84fbf6c1aa9d00e
SpenUtt <spencerutt8@gmail.com>
2023-01-24 16:42:25 +0200

    remove unused code

--- passport.js
+++ passport.js
@@ -49,18 +49,3 @@ passport.use(new JWTStrategy({
     return callback(error)
     });
 }));
-
-
-/*
-passport.use(new JWTStrategy ({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-    }, (jwtPayload, callback) => {
-        return Users.findById(jwtPayload.id)
-        .then((user) => {
-            return callback(null, user);
-        })
-        .catch((error) => {
-            return callback(error)
-        });
-    }));*/
