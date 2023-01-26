128fcf71a2ea71de0ac99da971daf8c03bb4dd38
SpenUtt <spencerutt8@gmail.com>
2023-01-26 11:45:03 +0100

    vercel setup

--- index.js
+++ index.js
@@ -228,6 +228,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
