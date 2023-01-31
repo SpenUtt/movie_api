1c4e959a155636a5e12e5518c04e88a8fcf96726
SpenUtt <spencerutt8@gmail.com>
2023-01-31 09:51:48 +0100

    remove authentication for /movies endpoint

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/documentation', (req, res) => {
 });
 
 //get all movies 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', /*passport.authenticate('jwt', { session: false }), */(req, res) => {
   Movies.find({}).then(movies => {
     res.status(200).send(movies);
   }).catch(e => {
